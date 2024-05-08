2605192eab7dfc6fbbfed1a19bda0c8126e32b9e
michaelehiosu <michael.ehiosumwen@student.nhlstenden.com>
2024-05-08 14:10:59 +0200

    Connected the App to Firebase

--- app/src/main/java/com/michael/viatoapp/activities/RegisterActivity.kt
+++ app/src/main/java/com/michael/viatoapp/activities/RegisterActivity.kt
@@ -3,10 +3,6 @@ package com.michael.viatoapp.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
-import com.michael.viatoapp.R
 import com.michael.viatoapp.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
