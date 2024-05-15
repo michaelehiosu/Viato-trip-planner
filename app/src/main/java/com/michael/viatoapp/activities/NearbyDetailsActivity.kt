a7d822774d24fc11d985ece4250073d2d22c9b82
michaelehiosu <michael.ehiosumwen@student.nhlstenden.com>
2024-05-15 17:39:43 +0200

    Created the UI structure of the nearby pages

---
+++ app/src/main/java/com/michael/viatoapp/activities/NearbyDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.michael.viatoapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.michael.viatoapp.R
+
+class NearbyDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nearby_details)
+    }
+}
