1e3fcd53f3694aed2d0562d7e510b0f3987416f0
michaelehiosu <michael.ehiosumwen@student.nhlstenden.com>
2024-05-16 16:29:52 +0200

    Finished the frontend of the react application

---
+++ app/src/main/java/com/michael/viatoapp/classes/Activities.kt
@@ -0,0 +1,7 @@
+package com.michael.viatoapp.classes
+
+class Activities(
+//var imageUrl: String,
+    var imageUrl : Int,
+var name: String,
+var description: String);
--- app/src/main/java/com/michael/viatoapp/fragments/NearbyFragment.kt
+++ app/src/main/java/com/michael/viatoapp/fragments/NearbyFragment.kt
@@ -5,6 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.michael.viatoapp.R
+import com.michael.viatoapp.classes.Activities
+import com.michael.viatoapp.classes.ActivityAdapter
 import com.michael.viatoapp.databinding.ActivityNearbyBinding
 
 class NearbyFragment : Fragment() {
@@ -13,5 +17,25 @@ class NearbyFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = ActivityNearbyBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.recyclerViewActivities.layoutManager = LinearLayoutManager(requireContext())
+
+        var activities = mutableListOf<Activities>(
+            Activities(R.drawable.redeemer, "Christ the Redeemer", "Type: Monuments and Memorials"),
+            Activities(R.drawable.redeemer, "Christ the Redeemer", "Type: Monuments and Memorials"),
+            Activities(R.drawable.redeemer, "Christ the Redeemer", "Type: Monuments and Memorials"),
+            Activities(R.drawable.redeemer, "Christ the Redeemer", "Type: Monuments and Memorials"),
+            Activities(R.drawable.redeemer, "Christ the Redeemer", "Type: Monuments and Memorials"))
+
+        bind(activities);
+    }
+
+    private fun bind(activities : MutableList<Activities>) {
+        val adapter = ActivityAdapter(activities)
+        binding.recyclerViewActivities.adapter = adapter
     }
 }
