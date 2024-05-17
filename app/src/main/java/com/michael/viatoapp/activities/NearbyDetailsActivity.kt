a16af3103b2aabb3b1a8a3bec93dbc1b974e0e0a
michaelehiosu <michael.ehiosumwen@student.nhlstenden.com>
2024-05-17 03:38:49 +0200

    completed the Nearby pages frontend, connected the app to google maps API

--- app/src/main/java/com/michael/viatoapp/activities/NearbyDetailsActivity.kt
+++ app/src/main/java/com/michael/viatoapp/activities/NearbyDetailsActivity.kt
@@ -1,12 +1,59 @@
 package com.michael.viatoapp.activities
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
 import com.michael.viatoapp.R
+import com.michael.viatoapp.databinding.ActivityNearbyBinding
+import com.michael.viatoapp.databinding.ActivityNearbyDetailsBinding
 
-class NearbyDetailsActivity : AppCompatActivity() {
+class NearbyDetailsActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var binding: ActivityNearbyDetailsBinding
+    private lateinit var mMap : GoogleMap
+    private lateinit var mapView: MapView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_nearby_details)
+        binding = ActivityNearbyDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+        mapView.onResume()
+        mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+        val targetLocation = LatLng(-22.9068, -43.1729)
+        val anotherLocation = LatLng(-22.9707, -43.1824) // Another location
+
+//        val zoomLevel = 15f
+        mMap.addMarker(MarkerOptions().position(targetLocation).title("Rio de Janiero"))
+        mMap.addMarker(MarkerOptions().position(anotherLocation).title("Another Location").icon(
+            BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)))
+
+
+
+        val boundsBuilder = LatLngBounds.Builder()
+        boundsBuilder.include(targetLocation)
+        boundsBuilder.include(anotherLocation)
+        val bounds = boundsBuilder.build()
+        val padding = 100 // Padding in pixels
+        val cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, padding)
+        mMap.moveCamera(cameraUpdate)
+
+        // Add a line connecting the two markers
+        mMap.addPolyline(PolylineOptions().add(targetLocation, anotherLocation).color(Color.BLUE))
     }
 }
--- app/src/main/java/com/michael/viatoapp/fragments/NearbyFragment.kt
+++ app/src/main/java/com/michael/viatoapp/fragments/NearbyFragment.kt
@@ -6,13 +6,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.MarkerOptions
 import com.michael.viatoapp.R
 import com.michael.viatoapp.classes.Activities
 import com.michael.viatoapp.classes.ActivityAdapter
 import com.michael.viatoapp.databinding.ActivityNearbyBinding
 
-class NearbyFragment : Fragment() {
+class NearbyFragment : Fragment(), OnMapReadyCallback {
     private lateinit var binding : ActivityNearbyBinding
+    private lateinit var mMap : GoogleMap
+    private lateinit var mapView: MapView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = ActivityNearbyBinding.inflate(inflater, container, false)
@@ -23,6 +32,10 @@ class NearbyFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.recyclerViewActivities.layoutManager = LinearLayoutManager(requireContext())
+        mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+        mapView.onResume()
+        mapView.getMapAsync(this)
 
         var activities = mutableListOf<Activities>(
             Activities(R.drawable.redeemer, "Christ the Redeemer", "Type: Monuments and Memorials"),
@@ -33,6 +46,17 @@ class NearbyFragment : Fragment() {
 
         bind(activities);
     }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+        val targetLocation = LatLng(-22.9068, -43.1729)
+        val zoomLevel = 15f
+        mMap.addMarker(MarkerOptions().position(targetLocation).title("Rio de Janiero"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(targetLocation, zoomLevel))
+    }
+
 
     private fun bind(activities : MutableList<Activities>) {
         val adapter = ActivityAdapter(activities)
