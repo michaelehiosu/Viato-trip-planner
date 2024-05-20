1a8ebd52b6392ee73015f428d7626eb34a3879f2
CaterinaAresti <111961519+CaterinaAresti@users.noreply.github.com>
2024-05-20 15:53:46 +0200

    added city overview, including flight and hotel recyclers

---
+++ app/src/main/java/com/michael/viatoapp/activities/CityOverviewActivity.kt
@@ -0,0 +1,13 @@
+package com.michael.viatoapp.activities
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.michael.viatoapp.R
+
+
+class CityOverviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_city_overview)
+
+    }
+}
