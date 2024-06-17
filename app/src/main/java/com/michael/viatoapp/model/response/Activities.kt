f19a8a2e9a3965cbb20d1fa7eacc06d7539b286f
iamjames5 <lennox.james@student.nhlstenden.com>
2024-06-17 14:26:12 +0200

    Nearby page now shows the activities that are in your range

--- app/src/main/java/com/michael/viatoapp/model/response/Activities.kt
+++ app/src/main/java/com/michael/viatoapp/model/response/Activities.kt
@@ -1,7 +1,7 @@
 package com.michael.viatoapp.model.response
 
 class Activities(
-//var imageUrl: String,
-    var imageUrl : Int,
+var imageUrl: String?,
+//    var imageUrl : Int,
 var name: String,
 var description: String);
